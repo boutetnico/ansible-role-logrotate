d0462988d8ddce30d585a60567d26990a03c4846
Nicolas Boutet <amd3002@gmail.com>
2022-09-08 09:24:49 +0200

    Update Ubuntu for testing + fix molecule lint issues (#1)
    * Update Ubuntu for testing + fix molecule lint issues
    
    * Fix lint issues
    
    * Fix

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -16,12 +16,13 @@ def test_packages_are_installed(host, name):
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file', [
-  ('kibana'),
+@pytest.mark.parametrize('file,user,group,mode', [
+  ('kibana', 'root', 'root', 0o644),
 ])
-def test_cron_files_exist(host, file):
+def test_cron_files_exist(host, file, user, group, mode):
     cron_file = host.file('/etc/logrotate.d/' + file)
     assert cron_file.exists
     assert cron_file.is_file
-    assert cron_file.user == 'root'
-    assert cron_file.group == 'root'
+    assert cron_file.user == user
+    assert cron_file.group == group
+    assert cron_file.mode == mode
