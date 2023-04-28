f8265e31f189284ac45c9bd2e861044b399a5685
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 11:06:57 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,22 +5,29 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('logrotate'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("logrotate"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file,user,group,mode', [
-  ('kibana', 'root', 'root', 0o644),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode",
+    [
+        ("kibana", "root", "root", 0o644),
+    ],
+)
 def test_cron_files_exist(host, file, user, group, mode):
-    cron_file = host.file('/etc/logrotate.d/' + file)
+    cron_file = host.file("/etc/logrotate.d/" + file)
     assert cron_file.exists
     assert cron_file.is_file
     assert cron_file.user == user
